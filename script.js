4eb96ec502a138c26c5cfc2b2c906c47023e2038
Daniel Woytitzki <danielwoytitzki@icloud.com>
2024-06-06 07:23:54 +0200

    added json

---
+++ script.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        "question": "Wie lautet der volle Name von Walt Disney?",
+        "answer_1": "Walter Ethan Disney",
+        "answer_2": "Walter Edward Disney",
+        "answer_3": "Walter Elias Disney",
+        "answer_4": "Walter Elmer Disney",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie hieß der Drache in dem Film Elliot, das Schmunzelmonster?",
+        "answer_1": "Puff",
+        "answer_2": "Figment",
+        "answer_3": "Mushu",
+        "answer_4": "Elliot",
+        "right_answer": 4
+    },
+    {
+        "question": "Welche Disney-Prinzessin singt das Lied Almost There?",
+        "answer_1": "Rapunzel",
+        "answer_2": "Tiana",
+        "answer_3": "Merida",
+        "answer_4": "Mulan",
+        "right_answer": 2
+    },
+    {
+        "question": "Was war der erste abendfüllende Animationsfilm, der jemals veröffentlicht wurde?",
+        "answer_1": "Pinocchio",
+        "answer_2": "Fantasia",
+        "answer_3": "Dumbo",
+        "answer_4": "Schneewittchen und die sieben Zwerge",
+        "right_answer": 4
+    },
+    {
+        "question": "Wie viele Welpen hatten Pongo und Perdita in 101 Dalmatiner?",
+        "answer_1": "15",
+        "answer_2": "17",
+        "answer_3": "21",
+        "answer_4": "99",
+        "right_answer": 1
+    },
+    {
+        "question": "Welcher Disney-Film war der erste, der eine Oscar-Nominierung für den besten Film erhielt?",
+        "answer_1": "Die Schöne und das Biest",
+        "answer_2": "Der König der Löwen",
+        "answer_3": "Aladdin",
+        "answer_4": "Bambi",
+        "right_answer": 1
+    },
+    {
+        "question": "In welchem Disney-Film gibt es einen Charakter namens Lucifer?",
+        "answer_1": "Aladdin",
+        "answer_2": "Cinderella",
+        "answer_3": "Die Hexe und der Zauberer",
+        "answer_4": "Schneewittchen und die sieben Zwerge",
+        "right_answer": 2
+    }
+];
